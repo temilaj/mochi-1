f64ffe6cef93f88e2d8f13a305d39eabcb8d5cdc
Erik Dunteman <44653944+erik-dunteman@users.noreply.github.com>
2024-11-30 19:58:55 -0800

    add longer timeout to preprocess (#105)
    * add timeout
    
    * bump
    
    * fix bump

--- contrib/modal/main.py
+++ contrib/modal/main.py
@@ -78,6 +78,7 @@ def download_weights():
         "/videos_prepared": videos_prepared_volume,
         "/weights": weights_volume,
     },
+    timeout=30*MINUTES,
     gpu="H100"
 )
 def preprocess():
@@ -127,7 +128,7 @@ def finetune():
         "/finetunes": finetunes_volume,
         "/outputs": outputs_volume,
     },
-    timeout=20*MINUTES,
+    timeout=30*MINUTES,
     gpu="H100"
 )
 class MochiLora():
